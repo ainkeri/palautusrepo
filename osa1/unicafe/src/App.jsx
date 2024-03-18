c3d30fa842069b4199dbbb66b66380bc42ceaffc
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-03-18 14:22:25 +0200

    Tehtävä 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,23 +5,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    const updatedAll = updatedGood + neutral + bad
+    setAll(updatedAll)
+    setAverage((updatedGood - bad)/updatedAll)
+    setPositive(updatedGood/updatedAll*100)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    const updatedAll = good + updatedNeutral + bad
+    setAll(updatedAll)
+    setAverage((good - bad)/updatedAll)
+    setPositive(good/updatedAll*100)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedAll = good + neutral + updatedBad
+    setAll(updatedAll)
+    setAverage((good - updatedBad)/updatedAll)
+    setPositive(good/updatedAll*100)
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <h2>statistics</h2>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
