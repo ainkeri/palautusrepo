28b33fb6ec95af1957abdd657d2818e9154a0a41
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-03-19 14:27:14 +0200

    Tehtävä 2.4

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,40 +1,74 @@
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course =>
+        <Courses key={course.id} courses={course} />)}
     </div>
   )
 }
 
-const Course = ({ course }) => {
-  console.log(course)
+const Courses = ({ courses }) => {
+  console.log(courses)
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
+    </div>
+  )
+}
+
+const Course = ({ courses }) => {
+  console.log(courses)
+  return (
+    <div>
+      <Header course={courses} />
+      <Content parts={courses.parts} />
+      <Total parts={courses.parts} />
     </div>
   )
 }
@@ -43,7 +77,7 @@ const Header = ({ course }) => {
   console.log(course.name)
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </div>
   )
 }
@@ -71,7 +105,7 @@ const Total = ({ parts }) => {
   const total = parts.reduce((sum, parts) => sum + parts.exercises, 0)
   return (
     <div>
-      <p><b>total of {total}</b></p>
+      <p><b>total of {total} exercises</b></p>
     </div>
   )
 }
