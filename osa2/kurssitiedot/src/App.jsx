62671cbf061fb5f590f09ec1fdf8ddbe50be0315
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-03-19 14:32:46 +0200

    Tehtävä 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./components/Course"
+
 const App = () => {
   const courses = [
     {
@@ -48,55 +50,6 @@ const App = () => {
     <div>
       {courses.map(course =>
         <Course key={course.id} courses={course} />)}
-    </div>
-  )
-}
-
-const Course = ({ courses }) => {
-  console.log(courses)
-  return (
-    <div>
-      <h1>Web development curriculum</h1>
-      <Header course={courses} />
-      <Content parts={courses.parts} />
-      <Total parts={courses.parts} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  console.log(course.name)
-  return (
-    <div>
-      <h2>{course.name}</h2>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, id) =>
-        <Part key={id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  console.log(part)
-  return (
-    <div>
-      <p>{part.name} {part.exercises}</p>
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, parts) => sum + parts.exercises, 0)
-  return (
-    <div>
-      <p><b>total of {total} exercises</b></p>
     </div>
   )
 }
