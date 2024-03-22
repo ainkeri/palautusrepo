dfabe4b3563c3e23ed70b01a4a043bede755d476
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-03-22 17:14:24 +0200

    Tehtävä 2.10

---
+++ osa2/puhelinluettelo/src/components/Add.jsx
@@ -0,0 +1,25 @@
+const Add = ({ onsubmit, value, onChange, value2, onChange2 }) => {
+    return (
+        <form onSubmit={onsubmit}>
+            <div>
+                name:
+                <input
+                    value={value}
+                    onChange={onChange}
+                />
+            </div>
+            <div>
+                number:
+                <input
+                    value={value2}
+                    onChange={onChange2}
+                />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+export default Add
