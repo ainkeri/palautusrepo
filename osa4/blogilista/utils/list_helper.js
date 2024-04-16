466df0afac491c3f9da6a2efb007fc0b13eb7cf8
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-16 21:45:29 +0200

    Tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
-    return 1
+   return 1
+}
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else if (blogs.length === 1) {
+        return blogs[0].likes
+    } else {
+        return blogs.map((blog) => blog.likes).reduce((blog, another) => blog + another)
+    }
+
 }
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
