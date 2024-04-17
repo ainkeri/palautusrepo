081d290c316320b2f08ee5ed7897c44be5729688
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-17 11:18:54 +0200

    Tehtävä 4.6*

--- osa4/blogilista/tests/dummy.test.js
+++ osa4/blogilista/tests/dummy.test.js
@@ -121,7 +121,47 @@ describe('max likes', () => {
 
     test('most likes correct', () => {
         const result = listHelper.favoriteBlog(listWithManyBlogs)
-        assert.strictEqual(result, "Canonical string reduction")
+        assert.deepStrictEqual(result, {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        })
     })
+})
 
+describe('max blogs', () => {
+    const listWithManyBlogs = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+          likes: 12,
+          __v: 0
+        }
+    ]
+
+    test('most blogs correct', () => {
+        const result = listHelper.mostBlogs(listWithManyBlogs)
+        assert.deepStrictEqual(result, {
+            author: "Edsger W. Dijkstra",
+            blogs: 2
+        })
+    })
 })
