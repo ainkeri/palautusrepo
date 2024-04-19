fb67593e75e37997d2534db06f4d086fd194e7d8
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-19 15:52:28 +0200

    Tehtävä 4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,32 +1,17 @@
 const { test, after, beforeEach } = require('node:test')
-const Blog = require('../models/blog')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
-
-const initialBlogs = [
-  {
-    title: "This is a title",
-    author: "This is an author",
-    url: "This is an url",
-    likes: 100,
-  },
-  {
-    title: "This is a title 2",
-    author: "This is an author 2",
-    url: "This is an url 2",
-    likes: 200,
-  },
-]
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -37,12 +22,43 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-
 test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
+
+test('a blog can be added', async () => {
+  const newBlog = {
+    title: 'Blogging is cool',
+    author: 'Blogger',
+    url: 'this is url',
+    likes: 20
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+  assert(titles.includes('Blogging is cool'))
+})
+
+test.only('id is defined', async () => {
+  const response = await api.get('/api/blogs')
+
+  const keys = Object.keys(response.body[0])
+
+  assert(keys.includes('id'))
+})
+
 
 after(async () => {
   await mongoose.connection.close()
