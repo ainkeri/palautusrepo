46780b287692c498972ee8252d3e82121a6753cf
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-22 20:36:34 +0200

    Tehtävä 4.15

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -8,8 +8,15 @@ blogRouter.get('/', async (request, response) => {
   })
   
 blogRouter.post('/', async (request, response) => {
-   const blog = new Blog(request.body)
+    const body = request.body
 
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+  
    if (blog.likes === undefined) {
      blog.likes = 0
    }
