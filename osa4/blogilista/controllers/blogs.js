64f22d71ac177c5ce58d204b655f72f251da6322
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-25 14:50:13 +0300

    Tehtävä 4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,35 +1,43 @@
-const blogRouter = require('express').Router()
 const mongoose = require('mongoose')
+const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+      .find({}).populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
   })
   
 blogRouter.post('/', async (request, response) => {
-    const body = request.body
+  const body = request.body
+
+  const user = await User.findById(body.user)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
   
-   if (blog.likes === undefined) {
-     blog.likes = 0
-   }
+  if (blog.likes === undefined) {
+    blog.likes = 0
+  }
    
-   if (blog.title === undefined) {
-     response.status(400).end()
-   } else if (blog.url === undefined) {
-     response.status(400).end()
-   } else {
+  if (blog.title === undefined) {
+    response.status(400).end()
+  } else if (blog.url === undefined) {
+    response.status(400).end()
+  } else {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    
     response.status(201).json(savedBlog)
-   }
+  }
 })
 
 blogRouter.delete('/:id', async (request, response) => {
