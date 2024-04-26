0d0e4fb15cd77c4c43869a6a30f5d06a9aa2ceab
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-26 14:55:00 +0300

    Tehtävä 4.22*

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,3 +1,6 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 const errorHandler = (error, request, response, next) => {
     if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
@@ -20,7 +23,20 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const getUser = await User.findById(decodedToken.id)
+  request.user = getUser
+
+  next()
+}
+
 module.exports = {
     errorHandler,
     tokenExtractor,
+    userExtractor,
 }
