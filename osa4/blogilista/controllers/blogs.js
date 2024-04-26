485093df1b5766428908e216b696274b870c0b7e
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-04-26 12:32:12 +0300

    Tehtävä 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,18 +2,24 @@ const mongoose = require('mongoose')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 require('express-async-errors')
 
-blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-      .find({}).populate('user', { username: 1, name: 1, id: 1 })
-    response.json(blogs)
-  })
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
   
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.user)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -38,6 +44,12 @@ blogRouter.post('/', async (request, response) => {
     
     response.status(201).json(savedBlog)
   }
+})
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1, id: 1 })
+  response.json(blogs)
 })
 
 blogRouter.delete('/:id', async (request, response) => {
