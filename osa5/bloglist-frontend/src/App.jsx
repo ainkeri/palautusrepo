41e4f13b9a7b3cad04909c743e0178bbde5a18bb
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-17 13:44:31 +0300

    Tehtävä 5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,6 +30,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -31,6 +43,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload()
   }
 
   const loginForm = () => (
@@ -64,7 +81,8 @@ const App = () => {
   const blogForm = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <p>{user.name} logged in</p>
+      <button onClick={handleLogout}>logout</button>
       <div>
         {blogs.map(blog => 
           <Blog key={blog.id} blog={blog} />
