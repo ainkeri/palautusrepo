1d6b7094c635a3f827c6c243905220c6702fc151
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-18 18:11:53 +0300

    Tehtävä 5.6

--- osa5/bloglist-frontend/src/components/BlogForm.jsx
+++ osa5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,27 +1,46 @@
-const BlogForm = ({ onSubmit, title, author, url, handleTitleChange, handleAuthorChange, handleUrlChange }) => {
+import { useState } from "react"
+
+const BlogForm = ({ createBlog }) => {
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+          title: newTitle,
+          author: newAuthor,
+          url: newUrl
+        })
+    
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+    }
+    
     return (
         <div>
             <h2>create new</h2>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={addBlog}>
                 <div>
                 title:
                     <input
-                    value={title}
-                    onChange={handleTitleChange}
+                    value={newTitle}
+                    onChange={event => setNewTitle(event.target.value)}
                 />
                 </div>
                 <div>
                 author:
                     <input
-                    value={author}
-                    onChange={handleAuthorChange}
+                    value={newAuthor}
+                    onChange={event => setNewAuthor(event.target.value)}
                 />
                 </div>
                 <div>
                 url:
                     <input
-                    value={url}
-                    onChange={handleUrlChange}
+                    value={newUrl}
+                    onChange={event => setNewUrl(event.target.value)}
                 />
                 </div>
                 <button type='submit'>create</button>
