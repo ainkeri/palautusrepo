cdde2853a1b557f746714ba939b55d86dcbf2c38
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-20 17:05:06 +0300

    Tehtävä 5.9*

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -64,7 +64,8 @@ blogRouter.put('/:id', async (request, response) => {
     likes: body.likes
   }
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true}).populate('user', { username: 1, name: 1, id: 1 })
+    
   response.json(updatedBlog)
 })
 
