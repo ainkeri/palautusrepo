d7d023a0bf7e73066736e836e0ea44945e790ec0
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-22 18:05:09 +0300

    Tehtävä 5.17

---
+++ osa5/bloglist-tests/tests/blog_app.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Inkeri Ahlström',
+        username: 'inksupinksu',
+        password: 'secret'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('log in to application')
+    await expect(locator).toBeVisible()
+  })
+})
