479f6fac12cfd06acd37c02dc785583f90556e78
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-22 18:16:16 +0300

    Tehtävä 5.18

--- osa5/bloglist-tests/tests/blog_app.spec.js
+++ osa5/bloglist-tests/tests/blog_app.spec.js
@@ -18,4 +18,25 @@ describe('Blog app', () => {
     const locator = await page.getByText('log in to application')
     await expect(locator).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('inksupinksu')
+        await page.getByTestId('password').fill('secret')
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Inkeri Ahlström logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+        await page.getByTestId('username').fill('inksupinksu')
+        await page.getByTestId('password').fill('wrong')
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Inkeri Ahlström logged in')).not.toBeVisible()
+        await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
+  })
 })
