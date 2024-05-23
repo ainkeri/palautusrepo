2ee89ae67c9ca017276a4b265238b180892a4860
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-23 12:47:10 +0300

    Tehtävä 5.19

--- osa5/bloglist-tests/tests/blog_app.spec.js
+++ osa5/bloglist-tests/tests/blog_app.spec.js
@@ -39,4 +39,25 @@ describe('Blog app', () => {
         await expect(page.getByText('wrong username or password')).toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+        await page.getByTestId('username').fill('inksupinksu')
+        await page.getByTestId('password').fill('secret')
+
+        await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+        await page.getByRole('button', { name: 'new blog' }).click()
+        
+        await page.getByTestId('title').fill('my first blog')
+        await page.getByTestId('author').fill('unknown')
+        await page.getByTestId('url').fill('https://myblogs.com/')
+
+        await page.getByRole('button', { name: 'create' }).click()
+
+        await expect(page.getByText('a new blog my first blog by unknown added')).toBeVisible()
+    })
+  })
 })
