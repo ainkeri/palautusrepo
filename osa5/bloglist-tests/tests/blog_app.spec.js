72b0f559f7ac1b8f1912f23f946b7e9c4cbd858c
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-24 12:50:58 +0300

    Tehtävä 5.23

--- osa5/bloglist-tests/tests/blog_app.spec.js
+++ osa5/bloglist-tests/tests/blog_app.spec.js
@@ -1,4 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { pathToFileURL } = require('url')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -29,20 +31,12 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('inksupinksu')
-        await page.getByTestId('password').fill('secret')
-
-        await page.getByRole('button', { name: 'login' }).click()
-
+        await loginWith(page, 'inksupinksu', 'secret')
         await expect(page.getByText('Inkeri Ahlström logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('inksupinksu')
-        await page.getByTestId('password').fill('wrong')
-
-        await page.getByRole('button', { name: 'login' }).click()
-
+        await loginWith(page, 'inksupinksu', 'wrong')
         await expect(page.getByText('Inkeri Ahlström logged in')).not.toBeVisible()
         await expect(page.getByText('wrong username or password')).toBeVisible()
     })
@@ -50,32 +44,16 @@ describe('Blog app', () => {
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-        await page.getByTestId('username').fill('inksupinksu')
-        await page.getByTestId('password').fill('secret')
-
-        await page.getByRole('button', { name: 'login' }).click()
+        await loginWith(page, 'inksupinksu', 'secret')
     })
 
     test('a new blog can be created', async ({ page }) => {
-        await page.getByRole('button', { name: 'new blog' }).click()
-        
-        await page.getByTestId('title').fill('my first blog')
-        await page.getByTestId('author').fill('unknown')
-        await page.getByTestId('url').fill('https://myblogs.com/')
-
-        await page.getByRole('button', { name: 'create' }).click()
-
+        await createBlog(page, 'my first blog', 'unknown', 'https://myblogs.com/')
         await expect(page.getByText('a new blog my first blog by unknown added')).toBeVisible()
     })
 
     test('a blog can be liked', async ({ page }) => {
-        await page.getByRole('button', { name: 'new blog' }).click()
-        
-        await page.getByTestId('title').fill('my another blog')
-        await page.getByTestId('author').fill('unknown again')
-        await page.getByTestId('url').fill('https://myblogs.com/')
-
-        await page.getByRole('button', { name: 'create' }).click()
+        await createBlog(page, 'my another blog', 'unknown again', 'https://myblogs.com/')
 
         await page.getByRole('button', { name: 'view' }).click()
         await page.getByRole('button', { name: 'like' }).click()
@@ -84,14 +62,7 @@ describe('Blog app', () => {
     })
 
     test('a blog created by user can be deleted by same user', async ({ page }) => {
-        await page.getByRole('button', { name: 'new blog' }).click()
-        
-        await page.getByTestId('title').fill('my another blog')
-        await page.getByTestId('author').fill('unknown again')
-        await page.getByTestId('url').fill('https://myblogs.com/')
-
-        await page.getByRole('button', { name: 'create' }).click()
-
+        await createBlog(page, 'my another blog', 'unknown again', 'https://myblogs.com/')
         await page.getByRole('button', { name: 'view' }).click()
 
         page.on('dialog', async dialog => {
@@ -101,34 +72,39 @@ describe('Blog app', () => {
         })
 
         await page.getByRole('button', { name: 'remove' }).click()
-
-        await expect(page.getByText('my another blog unknown againview')).not.toBeVisible()
+        await expect(page.getByText('my another blog unknown againhide')).not.toBeVisible()
     })
 
     describe('When another person logs in', () => {
         beforeEach(async ({ page }) => {
-            await page.getByRole('button', { name: 'new blog' }).click()
-            
-            await page.getByTestId('title').fill('my another blog')
-            await page.getByTestId('author').fill('unknown again')
-            await page.getByTestId('url').fill('https://myblogs.com/')
-
-            await page.getByRole('button', { name: 'create' }).click()
-            
-            await expect(page.getByText('a new blog my another blog by unknown again added')).toBeVisible()
-
+            await createBlog(page, 'my first blog', 'unknown', 'https://myblogs.com/')
             await page.getByRole('button', { name: 'logout' }).click()
         })
 
-        test.only('remove button is shown only if blog is created by user', async ({  page }) => {            
-            await page.getByTestId('username').fill('maijavirtanen')
-            await page.getByTestId('password').fill('salainen')
-
-            await page.getByRole('button', { name: 'login' }).click()
+        test('remove button is shown only if blog is created by user', async ({  page }) => {            
+            await loginWith(page, 'maijavirtanen', 'salainen')
 
             await page.getByRole('button', { name: 'view' }).click()
 
             await expect(page.getByText('remove')).not.toBeVisible()
+        })
+
+        test.only('blog with most likes is displayed first', async ({ page }) => {
+            await loginWith(page, 'maijavirtanen', 'salainen')
+
+            await createBlog(page, 'my another blog', 'another unknown', 'https://myblogs.com/another')
+            await page.getByText('my another blog another unknownview').waitFor()
+            await page.getByRole('button', { name: 'view' }).last().click()
+            await page.getByRole('button', { name: 'like' }).click()
+
+            await page.getByRole('button', { name: 'logout' }).click()
+
+            await loginWith(page, 'inksupinksu', 'secret')
+            
+            await page.getByRole('button', { name: 'view' }).first().click()
+            await page.getByText('https://myblogs.com/another').waitFor()
+
+            await expect(page.getByText('Maija Virtanen')).toBeVisible()
         })
     })
   })
