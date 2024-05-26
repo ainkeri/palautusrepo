99a88b2de896241073c72f6c20efcb3b2104adf4
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-26 17:47:35 +0300

    Tehtävä 6.9

--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -1,10 +1,21 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+import { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
+})
 
 const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('h'))
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
