3fc061d08c8633a7f681af9e150adc10077b40f5
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-05-27 12:34:07 +0300

    Tehtävä 6.10

--- osa6/redux-anecdotes/src/components/Filter.jsx
+++ osa6/redux-anecdotes/src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux"
-import { filterChange } from "../reducers/filterReducer"
+import filterReducer, { filterChange } from "../reducers/filterReducer"
 
 const Filter = () => {
     const dispatch = useDispatch()
