272c4f6acfb59cadad8e6908e8772f1f50768bf2
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-06-03 18:56:51 +0300

    Tehtävä 7.6

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -68,9 +68,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content } = useField("text")
+  const { reset: resetAuthor, ...author } = useField("text")
+  const { reset: resetInfo, ...info } = useField("text")
 
   const navigate = useNavigate()
 
@@ -90,9 +90,9 @@ const CreateNew = (props) => {
   }
 
   const handleReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
   
   return (
