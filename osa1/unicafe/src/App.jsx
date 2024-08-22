1991b9635b087472e1d9fcd75f99c9a6d2c2b802
Inkeri <ink eri.ahlstrom@helsinki.fi>
2024-08-22 13:32:01 +0300

    Setup

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,90 +1,10 @@
-import { useState } from 'react'
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = (props) => {
-  if (props.all === 0) {
-    return (
-      <div>
-        <h2>statistics</h2>
-        No feedback given
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h2>statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={props.good} />
-          <StatisticLine text="neutral" value={props.neutral} />
-          <StatisticLine text="bad" value={props.bad} />
-          <StatisticLine text="all" value={props.all} />
-          <StatisticLine text="average" value={props.average} />
-          <StatisticLine text="positive" value={props.positive + '%'} />
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
-
-  const handleGoodClick = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    const updatedAll = updatedGood + neutral + bad
-    setAll(updatedAll)
-    setAverage((updatedGood - bad)/updatedAll)
-    setPositive(updatedGood/updatedAll*100)
-  }
-
-  const handleNeutralClick = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    const updatedAll = good + updatedNeutral + bad
-    setAll(updatedAll)
-    setAverage((good - bad)/updatedAll)
-    setPositive(good/updatedAll*100)
-  }
-
-  const handleBadClick = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    const updatedAll = good + neutral + updatedBad
-    setAll(updatedAll)
-    setAverage((good - updatedBad)/updatedAll)
-    setPositive(good/updatedAll*100)
-  }
-
+  const [x, setX] = useState(1);
+  console.log(x);
   return (
     <div>
-      <h2>give feedback</h2>
-      <Button handleClick={handleGoodClick} text='good'/>
-      <Button handleClick={handleNeutralClick} text='neutral'/>
-      <Button handleClick={handleBadClick} text='bad'/>
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
+      {x}
+      <button onClick={() => setX(10)}>press</button>
     </div>
-  )
-}
-
-export default App
+  );
+};
