d66b99fde1f3bf9b4372cd2af6192cf7928e6ae1
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-11 17:32:42 +0200

    Exercise 9.3

--- osa9/typescript-dir/bmiCalculator.ts
+++ osa9/typescript-dir/bmiCalculator.ts
@@ -1,18 +1,46 @@
-const calculateBmi = (height: number, weight: number): string => {
-  const BMI = weight / (height / 100) ** 2;
+interface BmiValues {
+  height: number;
+  weight: number;
+}
 
-  if (BMI < 18.5) {
-    return "Underweight";
-  }
-  if (18.5 <= BMI && BMI <= 24.9) {
-    return "Normal weight";
-  }
-  if (25 <= BMI && BMI <= 29.9) {
-    return "Overweight";
-  }
-  if (BMI >= 30) {
-    return "Obese";
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers");
   }
 };
 
-console.log(calculateBmi(180, 74));
+const calculateBmi = (height: number, weight: number) => {
+  const BMI = weight / (height / 100) ** 2;
+
+  if (BMI < 18.5) {
+    console.log("Underweight");
+  }
+  if (18.5 <= BMI && BMI <= 24.9) {
+    console.log("Normal weight");
+  }
+  if (25 <= BMI && BMI <= 29.9) {
+    console.log("Overweight");
+  }
+  if (BMI >= 30) {
+    console.log("Obese");
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  calculateBmi(height, weight);
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
--- osa9/typescript-dir/exerciseCalculator.ts
+++ osa9/typescript-dir/exerciseCalculator.ts
@@ -8,17 +8,47 @@ interface Result {
   average: number;
 }
 
-const calculateExercises = (args: number[]): Result => {
+interface ExerciseDays {
+  trainingDays: number[];
+}
+
+const parseArguments2 = (args: string[]): ExerciseDays => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 15) throw new Error("Too many arguments");
+
+  let argsSliced = args.slice(2);
+  let numberDays = argsSliced.map((d) => {
+    return Number(d);
+  });
+  console.log(argsSliced);
+  const arr: number[] = [];
+
+  for (var i = 0; i < argsSliced.length; i++) {
+    if (!isNaN(numberDays[i])) {
+      arr.push(numberDays[i]);
+    } else {
+      throw new Error("Provided values were not numbers");
+    }
+  }
+  return {
+    trainingDays: arr,
+  };
+};
+
+const calculateExercises = (trainingDays: number[]): Result => {
+  let targetGoal = trainingDays[0];
   let sumOfTrainingDays = 0;
   let sumHours = 0;
   let successOrNot = false;
   let ratingOf = 0;
   let ratingOfDesc = "";
 
-  for (const day of args) {
-    if (day > 0) {
+  console.log(trainingDays);
+
+  for (var i = 0; i < trainingDays.length; i++) {
+    if (trainingDays[i] > 0) {
       sumOfTrainingDays += 1;
-      sumHours += day;
+      sumHours += trainingDays[i];
     }
   }
 
@@ -42,15 +72,23 @@ const calculateExercises = (args: number[]): Result => {
   }
 
   return {
-    periodLength: Number(args.length),
-    trainingDays: sumOfTrainingDays,
+    periodLength: trainingDays.length - 1,
+    trainingDays: sumOfTrainingDays - 1,
     success: successOrNot,
     rating: ratingOf,
     ratingDescription: ratingOfDesc,
-    target: 5,
-    average: sumHours / args.length,
+    target: targetGoal,
+    average: sumHours / trainingDays.length,
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
-console.log(calculateExercises([3, 0, 0, 0, 0, 3, 1]));
+try {
+  const { trainingDays } = parseArguments2(process.argv);
+  console.log(calculateExercises(trainingDays));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
